94f41ffc62b213059ae06de6b2dc4774064207d9
blacksnow233 <longyuhuhao@126.com>
2018-12-30 22:45:38 +0800

    add js type

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
         { text: '知识',
           items: [
             { text: 'JavaScript', link: '/knowledge/js/' },
-            { text: 'UI框架', link: '/lien-ui/' },
+            { text: 'css', link: '/lien-ui/' },
             { text: '关于', link: '/about/' }
           ] 
         },
@@ -18,32 +18,18 @@ module.exports = {
     ],
     sidebar: {
         '/knowledge/js/': [
-                    {
-                      title: 'javascript',
-                      children: [
-                        '',
-                        '1start-learn',
-                        '2vue-constructor',
-                        '3vue-example',
-                        '4vue-normalize',
-                        '5vue-merge',
-                        '6vue-init-start',
-                        '7vue-reactive',
-                        '8vue-reactive-dep-watch',
-                        '9vue-state-init',
-                        '80vue-compiler-start',
-                        '81vue-lexical-analysis',
-                        '82vue-parsing',
-                        '83vue-parsing-2',
-                        '84vue-codegen',
-                        '85vue-vdom',
-                        '86vue-vdom-patch'
-                      ]
-                    }
-                     ],
-        },
-    sidebarDepth: 2,
-    lastUpdated: 'Last Updated', 
+          {
+            title: 'javascript',
+            children: [
+              '',
+              '1.type',
+              '1.typeDetail'
+            ]
+          }
+        ],
+    },
+    sidebarDepth: 1,
+    // lastUpdated: 'Last Updated', 
 },
   
 }
